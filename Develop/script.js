584fcc54abdd88e5f470cad0baad5bc14e0f3aed
Jonathan Honda <hondahelix120@gmail.com>
2020-09-18 18:52:04 -0700

    made object passwordData and made prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var passwordData = {
+  numbers: "01234456789",
+  alpabet: "abcdefghijklmopqrstuvwxyz",
+  special: "[$&+,:;=?@#|'<>.-^*()%!]",
+
+};
+// console.log(passwordData.numbers);
+// console.log(passwordData.alpabet);
+// console.log(passwordData.special);
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -8,6 +17,12 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+function generatePassword(){
+  var pLen = prompt("Choose a length between 8 and 128");
+  var charType = prompt("choose lowercase = l, uppercase =u , numeric =n , and/or special characters = s ");
+  // console.log(pLen);
+  // console.log(charType);
 }
 
 // Add event listener to generate button
