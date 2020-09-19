38b62ffb332845229cca8baceca0cb66be95be36
Jonathan Honda <hondahelix120@gmail.com>
2020-09-18 22:19:22 -0700

    working version

--- Develop/script.js
+++ Develop/script.js
@@ -2,12 +2,8 @@
 var generateBtn = document.querySelector("#generate");
 
 var inputQ = ["lowercase? y/n","uppercase ? y/n","numeric? y/n","special characters? y/n"];
-var passwordData = {
-  numbers: "01234456789",
-  alpabet: "abcdefghijklmopqrstuvwxyz",
-  special: "[$&+,:;=?@#|'<>.-^*()%!]",
+var passwordData = ["abcdefghijklmopqrstuvwxyz","ABCDEFGHIJKLMNOPQRSTUV","01234456789","[$&+,:;=?@#|'<>.-^*()%!]"];
 
-};
 // console.log(passwordData.numbers);
 // console.log(passwordData.alpabet);
 // console.log(passwordData.special);
@@ -26,29 +22,69 @@ function generatePassword(){
     charType.push(prompt("Include " + inputQ[i]));
     console.log(charType);
   }
-  validInput(pLen,charType);
+  if (validInput(pLen,charType===true)){
+    var pass = make(pLen,charType);
+    console.log(pass);
+    return pass;
+  }
+}
+//makes password
+function make(x,y){
+  var data = "";
+  var charArray = [];
+  var made = "";
+  for(var j =0; j<4; j++){
+    if(y[j]==="y"){
+      data=data + passwordData[j];
+      charArray = data.split("");
+    }
+  }
+  for(var i =0; i<x; i++){
+    var data 
+    var dataLen = charArray.length;
+    //console.log(dataLen);
+    n = Math.floor(Math.random()*(dataLen));
+    console.log("-------------");
+    console.log(n);
+    made = made+charArray[n];
+  }
+  return made;
 }
 
+//checks to see if its a valid input
 function validInput(l,t){
   console.log(l);
   console.log(t);
   if((parseInt(l)<8) || (parseInt(l)>128)){
     alert("PICK A NUMBER 8 THROUGH 128!!!!");
     generatePassword();
-    //console.log("1 failed");
+    console.log("1 failed");
+  }
+  else{
+    return true;
   }
 
   for(var i=0; i<t.length; i++){
-    console.log(t[i]);
-    console.log(t[i]);
-    if((t[i]==="y") || (t[i]==="n")){
+    //console.log(t[i]);
+    console.log(t);
+    if((t[i]==="y") || (t[i]==="n") ){
       console.log("we made it");
-      return true;
+      if(t!==["n","n","n","n"]){
+        return true;
+      }
+      else{
+        console.log("3 failed")
+        alert("CANNOT ALL BE NO");
+        generatePassword();
+        return false;
+      }
+
     }
     else{
         alert("ANSWER y FOR YES AND n FOR NO!!");
         generatePassword();
-        console.log("2 failed")
+        //console.log("2 failed")
+        return false;
     }
   }
   
